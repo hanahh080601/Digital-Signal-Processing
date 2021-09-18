a8c4ce2f56b0eae259a7623a22b4ea216c9fc274
hanahh080601 <hanlehngoc080601@gmail.com>
2021-09-18 13:10:03 +0700

    fix playsound

--- Problem3.py
+++ Problem3.py
@@ -1,24 +1,26 @@
 from scipy.io import wavfile
 import numpy as np
 import matplotlib.pyplot as plt
-from playsound import playsound
+import sounddevice as sd
+import time
 
 Fs, data = wavfile.read('E:\DUT\Kì 5\Xử lí THS\Tín hiệu mẫu\LA001.wav')
-playsound('E:\DUT\Kì 5\Xử lí THS\Tín hiệu mẫu\LA001.wav')
 
 print(Fs)
-print(data.shape)
+print(data)
 
 n = np.linspace(0, data.shape[0] - 1, data.shape[0]).astype(int)
 
 plt.figure(figsize=(15,5))
-plt.stem(n, data, use_line_collection=True)
+plt.plot(n, data)
 
 Fs1 = Fs // 2
 Fs2 = Fs * 2
 
-wavfile.write("E:\DUT\Kì 5\Xử lí THS\Tín hiệu mẫu\div2.wav", Fs1, data)
-wavfile.write("E:\DUT\Kì 5\Xử lí THS\Tín hiệu mẫu\mul2.wav", Fs2, data)
+sd.play(data, Fs)
+sd.wait()
+sd.play(data, Fs1)
+sd.wait()
+sd.play(data, Fs2)
 
-playsound("E:\DUT\Kì 5\Xử lí THS\Tín hiệu mẫu\div2.wav")
-playsound("E:\DUT\Kì 5\Xử lí THS\Tín hiệu mẫu\mul2.wav")
+
