3fd1c66bb2488042d241d18c4a7ddcc5c942f3ed
hanahh080601 <hanlehngoc080601@gmail.com>
2021-09-18 22:10:30 +0700

    done prob4

--- Problem4.py
+++ Problem4.py
@@ -1,21 +1,27 @@
 from scipy.io import wavfile
 import numpy as np
-from math import *
+import math
 import matplotlib.pyplot as plt
+import sounddevice as sd
+import time
 
-A = 1
-F0 = 500
+A = 10
+F0 = 1000
 phi = 0
 
 Fs1 = 3*F0
+print(Fs1)
 Ts1 = 1/Fs1
-n1 = np.linspace(0,Fs1)
-x1 = A*np.cos(2*pi*F0*n1*Ts1 + phi)
+n1 = np.linspace(0,4)
+print(n1.shape)
+x1 = A*np.cos(2*math.pi*F0*n1*Ts1 + phi).astype(float)
+print(x1)
 
 Fs2 = 1.5*F0
+print(Fs2)
 Ts2 = 1/Fs2
-n2 = np.linspace(0,Fs2)
-x2 = A*np.cos(2*pi*F0*n2*Ts2 + phi)
+n2 = np.linspace(0,4)
+x2 = A*np.cos(2*math.pi*F0*n2*Ts2 + phi).astype(float)
 
 plt.subplot(2,1,1)
 plt.plot(x1)
@@ -24,6 +30,9 @@ plt.subplot(2,1,2)
 plt.plot(x2)
 
 
+sd.play(x1, Fs1)
+time.sleep(1)
+sd.play(x2, Fs2)
 
 
 
